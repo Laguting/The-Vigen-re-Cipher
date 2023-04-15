71ca4abc21da22453cd3e796e4067bb9d35256cc
Laguting <lagutingmariconjaneg@gmail.com>
2023-04-16 00:44:20 +0800

    Move countdown function inside the Y if yes is initialized

--- vingenere-cipher-chr()-and-ord().py
+++ vingenere-cipher-chr()-and-ord().py
@@ -18,17 +18,17 @@ print("\33[36m_\33[0m" * 177)
 start = input("\n\n\33[1m\33[33mDo you want to start the program?\U0001F481 \n Enter Y if 'Yes' and N if 'No': \33[0m\n")
 if start == "Y":
     print("\n\033[96m\33[4mThe Program is Starting...\033[0m\n")
+    def countdown(n):
+            if(n==0):
+                print ("\n\33[45m\033[3mThank you for your patience\033[0m" + "\33[45m\033[3m!\033[0m")
+            else:
+                print(n)
+                countdown(n-1)
+    countdown(5)
 elif start == "N":
     quit
 else:
     quit
-def countdown(n):
-    if(n==0):
-        print ("\33[45m\033[3mThank you for your patience\033[0m" + "\33[45m\033[3m!\033[0m")
-    else: 
-        print(n)
-        countdown(n-1)
-countdown(5)
 # Get the user's message and keyword
 # Check the equivalent numbers of the letters inputted (Message & Key)
 # Add the equivalent numbers of the letters inputted (Message & Key)
