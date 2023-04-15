c246174e43dcb3171a8684688f2a33c8ba171817
Laguting <lagutingmariconjaneg@gmail.com>
2023-04-15 23:31:25 +0800

    Inserting Pseudocodes

---
+++ vingenere-cipher-chr()-and-ord().py
@@ -0,0 +1,11 @@
+# Laguting, Maricon Jane G.
+# BSCpE 1-4
+# Task: The inputted message and keyword by the user which should be in all uppercase and no spaces to be ciphered using Vigenere cipher.
+# Initial Messages
+# Start the program
+# Get the user's message and keyword
+# Check the equivalent numbers of the letters inputted (Message & Key)
+# Add the equivalent numbers of the letters inputted (Message & Key)
+# Get the equivalent letters of the sum extracted from the Message & Key
+# Print the Ciphertext
+# Outro
